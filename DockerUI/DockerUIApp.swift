1fc25e6b156e10aa39866ffdaa5eedeca1ade16f
Edward Langley <el-github@elangley.org>
2025-03-26 17:51:38 -0700

    Initial Commit

---
+++ DockerUI/DockerUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DockerUIApp.swift
+//  DockerUI
+//
+//  Created by Edward Langley on 3/26/25.
+//
+
+import SwiftUI
+
+@main
+struct DockerUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
